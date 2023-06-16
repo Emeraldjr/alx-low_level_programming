01671537e4db267d81365ca21f1c1986fd08f736
emeraldjr <emeraldyoroki@gmail.com>
2023-06-16 00:30:20 -0700

    101-quote.c file edited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - prints exactly and thst piece of art is useful
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
-}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+       return (1);
+}       
