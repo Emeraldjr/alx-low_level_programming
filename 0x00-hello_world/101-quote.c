114830a1fd3d748c43e436b4f040b45a4bdb3dab
emeraldjr <emeraldyoroki@gmail.com>
2023-06-16 00:37:52 -0700

    101-quote.c file updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<unistd.h>
-
 /**
  * main - prints exactly and thst piece of art is useful
  *
@@ -10,4 +9,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-}       
+}
