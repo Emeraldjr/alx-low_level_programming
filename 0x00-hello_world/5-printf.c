6bed2d387eab75ebe3e5c6fa097743f6919ea2de
emeraldjr <emeraldyoroki@gmail.com>
2023-06-15 21:33:45 -0700

    5-printf.c file edited

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - This is a cis program that prints a line using printf functiom
+ * main - This is a cis program that prints a line using printf function
+ *
  * Return: 0 (Successful)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
 
