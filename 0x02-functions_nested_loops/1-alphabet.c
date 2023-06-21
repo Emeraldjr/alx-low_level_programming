cc1e952ddc8e1330ee405071e4cb5e92f51f18ce
emeraldjr <emeraldyoroki@gmail.com>
2023-06-21 16:29:24 -0700

    1-alphabet.c file fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,8 @@
  */
 void print_alphabet(void)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(1);
-	}
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
 	_putchar('\n');
-
 }
